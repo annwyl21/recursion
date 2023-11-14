e325f52d23a41116d149e28fc61730069c496c84
Ellen Houghton <ellen.a.ash@gmail.com>
2023-11-14 19:44:12 +0000

    separate recursion code and explanation

---
+++ recursion_code.py
@@ -0,0 +1,71 @@
+def doTasks(tasks, timeToWork, index=0, currentSum=0, currentCombination=[], depth=0):
+    # Indentation based on recursion depth
+    indent = "  " * depth  
+    # print(f"{indent}doTasks(index={index}, currentSum={currentSum}, currentCombination={currentCombination})")
+    if currentSum > timeToWork:
+         print(f"{indent}{currentSum} minutes TOO LONG, {currentCombination}\n")
+    else:
+        if currentCombination:
+            print(f"{indent}calculating... {currentCombination})")
+        else:
+            print(f"resetting...")
+
+    # Initialize a list to store combinations
+    combinations = []
+
+    # If the current sum equals the target time, add the current combination to the list of combinations.
+    if currentSum == timeToWork:
+        print(f"FOUND combination ({timeToWork} minutes): ", end="")
+        for index, item in enumerate(currentCombination):
+            if index < len(currentCombination) - 1:
+                print(f"{item}, ", end="")
+            else:
+                print(f"{item}\n")
+        # print(f"{indent}Found combination: {currentCombination}")
+        combinations.append(currentCombination)
+        return combinations
+
+    # If the sum exceeds the target time or no more tasks are left, return an empty list (base case).
+    if currentSum > timeToWork or index >= len(tasks):
+        return combinations
+
+    # Include the current task
+    combinations += doTasks(tasks, timeToWork, index + 1, currentSum + tasks[index]['duration'], currentCombination + [tasks[index]['name']], depth + 1)
+
+    # Exclude the current task
+    combinations += doTasks(tasks, timeToWork, index + 1, currentSum, currentCombination, depth + 1)
+
+    return combinations
+
+'''
+Recursion in `doTasks`
+
+1. **Base Cases**:
+    - The first `if` statement checks if the `currentSum` of task durations equals `timeToWork`. If it does, the current combination of tasks is a valid solution, so it's added to the `combinations` list. Then, the function returns this list.
+    - The second `if` statement is another base case that handles two scenarios: 
+        - If `currentSum` exceeds `timeToWork`, it means the current combination of tasks is not valid (as it takes more time than allowed), so an empty list is returned.
+        - If `index` is equal to or greater than the length of the `tasks` list, it means all tasks have been considered, and no further combinations are possible. In this case, too, an empty list is returned.
+
+    These base cases are essential for stopping the recursion. Without them, the function would keep calling itself indefinitely.
+
+2. **Recursive Calls**:
+    - The function makes two recursive calls for each task: one includes the current task (`index`) in the combination, and the other excludes it.
+    - **Including the current task**: 
+        - It increments `index` by 1 to move to the next task.
+        - It adds the `duration` of the current task to `currentSum`.
+        - It adds the `name` of the current task to `currentCombination`.
+        - The function then calls itself with these updated values.
+    - **Excluding the current task**: 
+        - It increments `index` by 1 but does not change `currentSum` or `currentCombination`.
+        - This represents the scenario where the current task is not part of the combination.
+        - Again, the function calls itself with these values.
+    - Both these calls explore different paths: one where the task is included in the final combination and one where it's not.
+
+3. **Combining Results**:
+    - The results (lists of combinations) from both recursive calls are added together using the `+=` operator. This is how the function aggregates all valid combinations that add up to `timeToWork`.
+
+4. **Return**:
+    - Finally, the function returns the `combinations` list, which contains all the valid combinations found through the recursive exploration.
+
+In summary, the recursive part of the `doTasks` function systematically explores all possible ways to combine tasks, so that their total duration matches `timeToWork`. It does this by considering each task both in and out of the combination and then aggregates all valid combinations found during this process.
+'''
--- recursion_tasklist.py
+++ recursion_tasklist.py
@@ -1,4 +1,6 @@
 from create_graph import create_graph_code
+from recursion_code import doTasks
+
 # Casidoo - Given a list of tasks, where each task has a duration, and a limited amount of available time to work, write a function to return the tasks that can be completed within the given time, without re-ordering the original list of tasks.
 
 # RELEVANT EXAMPLE OF RECURSION
@@ -14,44 +16,7 @@ tasks = [
 
 timeToWork = 6
 
-def doTasks(tasks, timeToWork, index=0, currentSum=0, currentCombination=[], depth=0):
-    # Indentation based on recursion depth
-    indent = "  " * depth  
-    # print(f"{indent}doTasks(index={index}, currentSum={currentSum}, currentCombination={currentCombination})")
-    if currentSum > timeToWork:
-         print(f"{indent}{currentSum} minutes TOO LONG, {currentCombination}\n")
-    else:
-        if currentCombination:
-            print(f"{indent}calculating... {currentCombination})")
-        else:
-            print(f"resetting...")
-
-    # Initialize a list to store combinations
-    combinations = []
-
-    # If the current sum equals the target time, add the current combination to the list of combinations.
-    if currentSum == timeToWork:
-        print(f"FOUND combination ({timeToWork} minutes): ", end="")
-        for index, item in enumerate(currentCombination):
-            if index < len(currentCombination) - 1:
-                print(f"{item}, ", end="")
-            else:
-                print(f"{item}\n")
-        # print(f"{indent}Found combination: {currentCombination}")
-        combinations.append(currentCombination)
-        return combinations
-
-    # If the sum exceeds the target time or no more tasks are left, return an empty list (base case).
-    if currentSum > timeToWork or index >= len(tasks):
-        return combinations
-
-    # Include the current task
-    combinations += doTasks(tasks, timeToWork, index + 1, currentSum + tasks[index]['duration'], currentCombination + [tasks[index]['name']], depth + 1)
-
-    # Exclude the current task
-    combinations += doTasks(tasks, timeToWork, index + 1, currentSum, currentCombination, depth + 1)
-
-    return combinations
+combinations = doTasks(tasks, timeToWork=6)
 
 # To get the desired output format
 def formatOutput(combinations):
@@ -60,7 +25,6 @@ def formatOutput(combinations):
         output[i] = combo
     return output
 
-combinations = doTasks(tasks, timeToWork=6)
 formatted_output = formatOutput(combinations)
 # {1: ['Task 1', 'Task 2'], 2: ['Task 4', 'Task 5'], 3: ['Task 2', 'Task 5', 'Task 6']}
 
@@ -76,38 +40,9 @@ for option, listoftasks in formatted_output.items():
 # Use the formatted output to generate the Graphviz code
 create_graph_code(formatted_output)
 
-'''
-Recursion in `doTasks`
 
-1. **Base Cases**:
-    - The first `if` statement checks if the `currentSum` of task durations equals `timeToWork`. If it does, the current combination of tasks is a valid solution, so it's added to the `combinations` list. Then, the function returns this list.
-    - The second `if` statement is another base case that handles two scenarios: 
-        - If `currentSum` exceeds `timeToWork`, it means the current combination of tasks is not valid (as it takes more time than allowed), so an empty list is returned.
-        - If `index` is equal to or greater than the length of the `tasks` list, it means all tasks have been considered, and no further combinations are possible. In this case, too, an empty list is returned.
 
-    These base cases are essential for stopping the recursion. Without them, the function would keep calling itself indefinitely.
-
-2. **Recursive Calls**:
-    - The function makes two recursive calls for each task: one includes the current task (`index`) in the combination, and the other excludes it.
-    - **Including the current task**: 
-        - It increments `index` by 1 to move to the next task.
-        - It adds the `duration` of the current task to `currentSum`.
-        - It adds the `name` of the current task to `currentCombination`.
-        - The function then calls itself with these updated values.
-    - **Excluding the current task**: 
-        - It increments `index` by 1 but does not change `currentSum` or `currentCombination`.
-        - This represents the scenario where the current task is not part of the combination.
-        - Again, the function calls itself with these values.
-    - Both these calls explore different paths: one where the task is included in the final combination and one where it's not.
-
-3. **Combining Results**:
-    - The results (lists of combinations) from both recursive calls are added together using the `+=` operator. This is how the function aggregates all valid combinations that add up to `timeToWork`.
-
-4. **Return**:
-    - Finally, the function returns the `combinations` list, which contains all the valid combinations found through the recursive exploration.
-
-In summary, the recursive part of the `doTasks` function systematically explores all possible ways to combine tasks, so that their total duration matches `timeToWork`. It does this by considering each task both in and out of the combination and then aggregates all valid combinations found during this process.
-'''
+# ALTERNATIVE SOLUTION
 
 # Co-pilot solution is superb with its use of lambda as a sorting key however, it only offers 1 selection of tasks.  I wanted to see if I could get all the tasks that could be completed within the given time.
 # Sort tasks by duration, shortest to longest, then iterate through the list of tasks.  If the timeToWork is greater than or equal to the duration of the task, add the task to the completedTasks list and subtract the duration of the task from the timeToWork.  Return the completedTasks list.
