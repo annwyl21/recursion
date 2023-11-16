344d2aa299a1f860f9333f331a2f9035b723c6ea
Ellen Houghton <ellen.a.ash@gmail.com>
2023-11-16 16:32:46 +0000

    add comments

--- recursion.py
+++ recursion.py
@@ -2,20 +2,21 @@ import graphviz as gv
 
 def doTasks(tasks, dot, timeToWork, index=0, currentSum=0, currentCombination=[], depth=0, last_node='root'):
 	"""
-	Recursive function to find combinations of tasks that fit within a specified time limit.
+	Recursive function to find combinations of tasks that fit within a specified time limit
 
-	:param tasks: List of tasks where each task is a dictionary with 'name' and 'duration'.
-	:param dot: Graphviz graph object for visualization.
-	:param timeToWork: Total time limit for task combinations.
-	:param index: Current index in the tasks list.
-	:param currentSum: Current sum of durations in the current combination.
-	:param currentCombination: Current combination of tasks being considered.
-	:param depth: Current depth of recursion.
-	:param last_node: ID of the last node added to the graph.
+	:param tasks: List of tasks where each task is a dictionary with 'name' and 'duration'
+	:param dot: Graphviz graph object for visualization
+	:param timeToWork: Total time limit for task combinations
+	:param index: Current index in the tasks list
+	:param currentSum: Current sum of durations in the current combination
+	:param currentCombination: Current combination of tasks being considered
+	:param depth: Current depth of recursion
+	:param last_node: ID of the last node added to the graph
 	"""
 
-	# Base case: Exceeding time limit or no more tasks to consider
-	'''Base Case: The function returns an empty list if the current sum of task durations (currentSum) exceeds timeToWork or if there are no more tasks to consider (index >= len(tasks)).'''
+	# Base case: Exceeding time limit or no more tasks to consider thus preventing an infinite loop
+	'''Base Case: The function returns an empty list if the current sum of task durations (currentSum) exceeds timeToWork or if there are no more tasks to consider (index >= len(tasks)).
+	When designing a recursive function, defining the base case(s) is one of the first and most important steps. The recursive step (where the function calls itself) should always work towards reaching this base case.'''
 	if currentSum > timeToWork or index >= len(tasks):
 		return []
 
@@ -31,7 +32,7 @@ def doTasks(tasks, dot, timeToWork, index=0, currentSum=0, currentCombination=[]
 		dot.node(node_id_include, node_name, color='darkgreen', fillcolor='lightgreen')
 		dot.edge(last_node, node_id_include, color='green')
 		
-		# Recursive call - including the current task
+		# Recursive call - including the current task (do this chore now, add it and move on)
 		combinations = doTasks(tasks, dot, timeToWork, index + 1, currentSum + tasks[index]['duration'], new_combination_include, depth + 1, node_id_include)
 	else:
 		combinations = []
@@ -51,7 +52,7 @@ def doTasks(tasks, dot, timeToWork, index=0, currentSum=0, currentCombination=[]
 		dot.node(node_id_exclude, node_name, style='filled, dashed', color='orange', fillcolor='yellow')
 	dot.edge(last_node, node_id_exclude, color='grey')
 
-	# Recursive call - excluding the current task
+	# Recursive call - excluding the current task (bank the chore for later, skip it and move on)
 	combinations += doTasks(tasks, dot, timeToWork, index + 1, currentSum, new_combination_exclude, depth + 1, node_id_exclude)
 
 	'''Visualization: Each recursive call adds nodes and edges to the Graphviz graph, visualizing the decision tree of including or excluding tasks.'''
